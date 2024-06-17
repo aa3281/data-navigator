e168719c44ce3e618d0c742317d53b8c9d9a1e1a
Alex A <70526185+aa3281@users.noreply.github.com>
2024-06-17 15:30:41 +0800

    Add Navigation Bar

---
+++ src/fileSearch.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const FileSearch = () => {
+  return (
+    <div className="file-search">
+      <h2>File Search</h2>
+      <p>This is the File Search component.</p>
+    </div>
+  );
+};
+
+export default FileSearch;
